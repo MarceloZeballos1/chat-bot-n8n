d7b3632600c94f4e11891855a8e9027f9bc3f3e6
Diego Garcia <dracmil4@users.noreply.github.com>
2025-05-07 10:15:19 -0400

    Cambios frontend, en index.html y se agrego un style.css, y se agrego el index.js se agrego una ruta para que directamente se redirija hacia index.html

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+
+const path = require('path');
+
+// Servir archivos estáticos desde la raíz del proyecto
+app.use(express.static(path.join(__dirname)));
+
+// Ruta raíz para enviar el index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
 app.use(bodyParser.json());
 
 // Usamos las rutas
